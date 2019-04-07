802b69e05a929867cc5692deadf3aede6ef3b8cf
Donnyves Laroque <donnyves@gmail.com>
2019-04-07 00:03:42 -0700

    Added a unit test for Genre.

---
+++ CollegeBookRental/Models/Abstract/IGenre.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+using Moq;
+
+
+namespace CollegeBookRental.Models.Abstract
+{
+    public interface IGenre
+    {
+        IEnumerable<Genre> Genres { get; }
+    }
+}
+
+
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using CollegeBookRental.Models;
+using CollegeBookRental.Models.Abstract;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Create_New_Genres()
+        {
+            //Arrange
+            Mock<IGenre> mock = new Mock<IGenre>();
+            mock.Setup(m => m.Genres).Returns(new Genre[]
+            { new Genre { Id = 1, Name = "Action"},
+                new Genre {Id =2,Name = "Comedy"}
+
+            });
+
+        }
+    }
+}
